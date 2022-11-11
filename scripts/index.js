995a3f50ad0224e7ce42b7f7d0828c56e2b620a8
Ilya Semenov <askop95@yandex.ru>
2022-11-11 16:30:52 +0300

    добавляются данные из формы

--- scripts/index.js
+++ scripts/index.js
@@ -25,6 +25,8 @@ const initialCards = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
+
+// Добавляет карточку из Массива
 initialCards.forEach((item) => {
   // Создадим экземпляр карточки
   const card = new Card(item);
@@ -36,125 +38,139 @@ initialCards.forEach((item) => {
 });
 
 
-// // переменные для первого попапа
-// const profilePopup = document.querySelector(".popup_profile");
-// const buttonEditProfile = document.querySelector(".profile__edit-button");
-// const gapProfileName = document.querySelector(".profile__title");
-// const gapProfileJob = document.querySelector(".profile__subtitle");
-// // переменные для второго попапа
-// const buttonOpenAddCard = document.querySelector(".profile__add-button");
-// const popupAddCardOpen = document.querySelector(".popup_add-card");
-// // Находим форму в DOM
-// const profileForm = document.querySelector(".popup__container_type_profile"); // Воспользуйтесь методом querySelector()
-// // Находим поля формы в DOM
-// const nameInput = profileForm.querySelector(".popup__input_profilename"); // Воспользуйтесь инструментом .querySelector()
-// const jobInput = profileForm.querySelector(".popup__input_profilejob"); // Воспользуйтесь инструментом .querySelector()
+// Добавляет карточку из полей формы добавления
+//Обработчики событий
+const handleSubmitAddCardForm = (event) => {
+  event.preventDefault();
+  const userCard = new Card({
+    name: inputTitleAddCardForm.value,
+    link: inputImageAddCardForm.value,
+  });
+  const userCardElement = userCard.generateCard();
+    // Добавляем в DOM
+  document.querySelector(".elements__cards").prepend(userCardElement);
+  event.target.reset();
+  closePopup(popupAddCardOpen);
+};
 
-// // ФУНКЦИИ
-// // Если нажать на ESCAPE то закроем поп ап
-// function closeByEscape(evt) {
-//   if (evt.key === "Escape") {
-//     const openedPopup = document.querySelector(".popup_opened");
-//     closePopup(openedPopup);
-//   }
-// }
-// // Общая функция для открытия и закрытия поп ап
 
-// function openPopup(popup) {
-//   popup.classList.add("popup_opened");
-//   //добавляем слушатель, который отслеживает нажати esc
-//   document.addEventListener("keydown", closeByEscape);
-// }
 
-// function closePopup(popup) {
-//   popup.classList.remove("popup_opened");
-//   //отслеживаем нажатие esc и удаляем слушатель, который был добавлен при открытии
-//   document.removeEventListener("keydown", closeByEscape);
-// }
 
-// //Единая функция для закрытия поп ап
-// // находим все крестики проекта по универсальному селектору
-// const closeButtons = document.querySelectorAll(".popup__close");
 
-// closeButtons.forEach((button) => {
-//   // находим 1 раз ближайший к крестику попап
-//   const popup = button.closest(".popup");
 
-//   // Если нажать на оверлей то закроем поп ап
-//   popup.addEventListener("click", function (e) {
-//     if (e.target == e.currentTarget) closePopup(popup);
-//   });
 
-//   // устанавливаем обработчик закрытия на крестик
-//   button.addEventListener("click", function (e) {
-//     closePopup(popup);
-//   });
-// });
 
-// //Функции частная для Profile Popup
-// function saveInputDataProfile() {
-//   openPopup(profilePopup);
-//   nameInput.value = gapProfileName.textContent;
-//   jobInput.value = gapProfileJob.textContent;
-// }
 
-// //СЛУШАТЕЛИ
-// // Слушатели в форме в Profile
-// buttonEditProfile.addEventListener("click", saveInputDataProfile);
+// переменные для первого попапа
+const profilePopup = document.querySelector(".popup_profile");
+const buttonEditProfile = document.querySelector(".profile__edit-button");
+const gapProfileName = document.querySelector(".profile__title");
+const gapProfileJob = document.querySelector(".profile__subtitle");
+// переменные для второго попапа
+const buttonOpenAddCard = document.querySelector(".profile__add-button");
+const popupAddCardOpen = document.querySelector(".popup_add-card");
+// Находим форму в DOM
+const profileForm = document.querySelector(".popup__container_type_profile"); // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+const nameInput = profileForm.querySelector(".popup__input_profilename"); // Воспользуйтесь инструментом .querySelector()
+const jobInput = profileForm.querySelector(".popup__input_profilejob"); // Воспользуйтесь инструментом .querySelector()
 
-// // Слушатели формы addCard
-// buttonOpenAddCard.addEventListener("click", () => {
-//   openPopup(popupAddCardOpen);
-// });
+// ФУНКЦИИ
+// Если нажать на ESCAPE то закроем поп ап
+function closeByEscape(evt) {
+  if (evt.key === "Escape") {
+    const openedPopup = document.querySelector(".popup_opened");
+    closePopup(openedPopup);
+  }
+}
+// Общая функция для открытия и закрытия поп ап
 
-// // Обработчик «отправки» формы, хотя пока
-// // она никуда отправляться не будет
-// function handleProfileFormSubmit(evt) {
-//   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+  //добавляем слушатель, который отслеживает нажати esc
+  document.addEventListener("keydown", closeByEscape);
+}
 
-//   // Получите значение полей jobInput и nameInput из свойства value
-//   // Выберите элементы, куда должны быть вставлены значения полей
-//   // Вставьте новые значения с помощью textContent
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
+  //отслеживаем нажатие esc и удаляем слушатель, который был добавлен при открытии
+  document.removeEventListener("keydown", closeByEscape);
+}
 
-//   gapProfileName.textContent = nameInput.value;
-//   gapProfileJob.textContent = jobInput.value;
-//   closePopup(profilePopup);
-// }
-// // Прикрепляем обработчик к форме:
-// // он будет следить за событием “submit” - «отправка»
-// profileForm.addEventListener("submit", handleProfileFormSubmit);
+//Единая функция для закрытия поп ап
+// находим все крестики проекта по универсальному селектору
+const closeButtons = document.querySelectorAll(".popup__close");
 
-// ///шаблоны
-// //использовал в кард
-// const cardTemplate = document
-//   .querySelector("#template-card")
-//   .content.querySelector(".element");
+closeButtons.forEach((button) => {
+  // находим 1 раз ближайший к крестику попап
+  const popup = button.closest(".popup");
 
-// // DOM элементы
-// const cardsContainer = document.querySelector(".elements__cards");
-// const popupAddCardForm = document.querySelector(
-//   ".popup__container_type_add-card"
-// );
-// const inputTitleAddCardForm = document.querySelector(
-//   ".popup__input_titleaddcard"
-// );
-// const inputImageAddCardForm = document.querySelector(
-//   ".popup__input_imageaddcard"
-// );
-// const popupShowImage = document.querySelector(".popup-show-image");
-// const popupShowImageSelected = document.querySelector(".popup__image-selected");
-// const popupShowImageDescription = document.querySelector(".popup__description");
+  // Если нажать на оверлей то закроем поп ап
+  popup.addEventListener("click", function (e) {
+    if (e.target == e.currentTarget) closePopup(popup);
+  });
 
-// //Обработчики событий
-// const handleSubmitAddCardForm = (event) => {
-//   event.preventDefault();
-//   renderCard({
-//     name: inputTitleAddCardForm.value,
-//     link: inputImageAddCardForm.value,
-//   });
-//   event.target.reset();
-//   closePopup(popupAddCardOpen);
-// };
+  // устанавливаем обработчик закрытия на крестик
+  button.addEventListener("click", function (e) {
+    closePopup(popup);
+  });
+});
+
+//Функции частная для Profile Popup
+function saveInputDataProfile() {
+  openPopup(profilePopup);
+  nameInput.value = gapProfileName.textContent;
+  jobInput.value = gapProfileJob.textContent;
+}
+
+//СЛУШАТЕЛИ
+// Слушатели в форме в Profile
+buttonEditProfile.addEventListener("click", saveInputDataProfile);
+
+// Слушатели формы addCard
+buttonOpenAddCard.addEventListener("click", () => {
+  openPopup(popupAddCardOpen);
+});
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+  // Получите значение полей jobInput и nameInput из свойства value
+  // Выберите элементы, куда должны быть вставлены значения полей
+  // Вставьте новые значения с помощью textContent
+
+  gapProfileName.textContent = nameInput.value;
+  gapProfileJob.textContent = jobInput.value;
+  closePopup(profilePopup);
+}
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+profileForm.addEventListener("submit", handleProfileFormSubmit);
+
+///шаблоны
+//использовал в кард
+const cardTemplate = document
+  .querySelector("#template-card")
+  .content.querySelector(".element");
+
+// DOM элементы
+const cardsContainer = document.querySelector(".elements__cards");
+const popupAddCardForm = document.querySelector(
+  ".popup__container_type_add-card"
+);
+const inputTitleAddCardForm = document.querySelector(
+  ".popup__input_titleaddcard"
+);
+const inputImageAddCardForm = document.querySelector(
+  ".popup__input_imageaddcard"
+);
+const popupShowImage = document.querySelector(".popup-show-image");
+const popupShowImageSelected = document.querySelector(".popup__image-selected");
+const popupShowImageDescription = document.querySelector(".popup__description");
+
+
 // // обработчик который удаляет карточку
 // const handleDeleteCard = (evt) => {
 //   evt.target.closest(".element").remove();
@@ -165,13 +181,13 @@ initialCards.forEach((item) => {
 //   evt.target.classList.toggle("element__liked");
 // };
 
-// // обработчик событий открывет на весь экран фотку
-// const handleOpenImage = (cardData) => {
-//   openPopup(popupShowImage);
-//   popupShowImageSelected.src = cardData.link;
-//   popupShowImageSelected.alt = cardData.name;
-//   popupShowImageDescription.textContent = cardData.name;
-// };
+// обработчик событий открывет на весь экран фотку
+const handleOpenImage = (cardData) => {
+  openPopup(popupShowImage);
+  popupShowImageSelected.src = cardData.link;
+  popupShowImageSelected.alt = cardData.name;
+  popupShowImageDescription.textContent = cardData.name;
+};
 
 // //Генерация карточки
 // const generateCard = (cardData) => {
@@ -192,7 +208,7 @@ initialCards.forEach((item) => {
 //   return newCard;
 // };
 
-// //Рендер карточки
+//Рендер карточки
 // const renderCard = (cardData) => {
 //   cardsContainer.prepend(generateCard(cardData));
 // };
@@ -202,4 +218,6 @@ initialCards.forEach((item) => {
 //   renderCard(cardData);
 // });
 
-// popupAddCardForm.addEventListener("submit", handleSubmitAddCardForm);
+
+// Обработчик добавления карточки
+popupAddCardForm.addEventListener("submit", handleSubmitAddCardForm);
